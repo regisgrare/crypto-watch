519260b383d140b1d9fc8d4a398021d21ede3200
Régis GRARE <regisgrare@hotmail.fr>
2022-04-07 13:42:30 +0200

    table OK

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import GlobalChart from './components/GlobalChart';
 import HeaderInfos from './components/HeaderInfos';
+import Table from './components/Table';
+
 
 const App = () => {
   const [coinsData, setCoinsData] = useState([])
@@ -17,6 +19,7 @@ const App = () => {
         <HeaderInfos />
         <GlobalChart coinsData={coinsData} />
       </header>
+      <Table coinsData={coinsData} />
     </div>
   );
 };
---
+++ src/components/ToTop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ToTop = () => {
+    return (
+        <div className="top">
+            <img src="./assets/arrow-icon.svg" alt="arrow" />
+        </div>
+    );
+};
+
+export default ToTop;
